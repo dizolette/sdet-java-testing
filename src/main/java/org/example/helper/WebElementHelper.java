d3223b1a127f3a5552e3a03ef67cdb1121b8651c
dizolette <albertusdwie@gmail.com>
2024-02-05 19:55:00 +0700

    feat(base): create web driver wait of element presence

--- src/main/java/org/example/helper/WebElementHelper.java
+++ src/main/java/org/example/helper/WebElementHelper.java
@@ -4,12 +4,20 @@ import org.example.base.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class WebElementHelper {
 
     WebDriver driver = DriverFactory.getDriver();
+
+    public WebElement waitPresenceOfElement(By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
 
     public WebElement findElementById(String id) {
         return driver.findElement(By.id(id));
@@ -25,5 +33,15 @@ public class WebElementHelper {
 
     public List<WebElement> findElementsByClassName(String className) {
         return driver.findElements(By.className(className));
+    }
+
+    public void clickOn(By locator, int timeoutInSeconds){
+        WebElement element = waitPresenceOfElement(locator, timeoutInSeconds);
+        element.click();
+    }
+
+    public void sendKeysOn(By locator, int timeoutInSeconds, String inputText){
+        WebElement element = waitPresenceOfElement(locator, timeoutInSeconds);
+        element.sendKeys(inputText);
     }
 }
